34d0dd7a79de032ea7202ab2d1cca453e1e0e8dd
bonnechance989 <109989716+bonnechance989@users.noreply.github.com>
2023-10-22 10:48:27 -0800

    JobQuest Calculator 2

---
+++ force-app/main/default/lwc/jobQuestCalc2/jobQuestCalc2.js
@@ -0,0 +1,61 @@
+import { LightningElement, track, api } from 'lwc';
+import calculateTakeHomePay from '@salesforce/apex/JobApplicationTriggerHandlerHelper2.calculateTakeHomePay';
+
+export default class JobQuestCalculator extends LightningElement {
+    @track num1 = 0;
+    @track result = 0;
+    @track formattedResult = '$0.00';  // Initialize with default value
+    @track filingStatusValue = 'single';
+
+    get options() {
+        return [
+            { label: 'Single', value: 'single' },
+            { label: 'Married Filing Jointly', value: 'jointly' }
+        ];
+    }
+
+    onInputChangeHandler(event) {
+        this.num1 = event.target.value;
+    }
+
+    onFilingStatusChange(event) {
+        this.filingStatusValue = event.detail.value;
+    }
+
+    formatCurrency(value) {
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
+    }
+
+    calculateMonthlyPay() {
+        calculateTakeHomePay({ salary: this.num1, filingStatus: this.filingStatusValue, period: 'monthly' })
+            .then(result => {
+                this.result = result;
+                this.formattedResult = this.formatCurrency(this.result);  // format and set to formattedResult
+            })
+            .catch(error => {
+                console.error('Error fetching take-home pay:', error);
+            });
+    }
+
+    calculateAnnualPay() {
+        calculateTakeHomePay({ salary: this.num1, filingStatus: this.filingStatusValue, period: 'annual' })
+            .then(result => {
+                this.result = result;
+                this.formattedResult = this.formatCurrency(this.result);  // format and set to formattedResult
+            })
+            .catch(error => {
+                console.error('Error fetching take-home pay:', error);
+            });
+    }
+
+    calculateWeeklyPay() {
+        calculateTakeHomePay({ salary: this.num1, filingStatus: this.filingStatusValue, period: 'weekly' })
+            .then(result => {
+                this.result = result;
+                this.formattedResult = this.formatCurrency(this.result);  // format and set to formattedResult
+            })
+            .catch(error => {
+                console.error('Error fetching take-home pay:', error);
+            });
+    }
+}
